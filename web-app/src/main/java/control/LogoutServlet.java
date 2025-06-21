3523890eece80300758713075eb28848bc5f5f31
f-niemiec <f.niemiec@studenti.unisa.it>
2025-06-22 01:53:41 +0200

    Implemented a servlet for the Logout

---
+++ web-app/src/main/java/control/LogoutServlet.java
@@ -0,0 +1,33 @@
+package control;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public LogoutServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		if(session != null) {
+			session.invalidate();
+		}
+		response.sendRedirect(request.getContextPath() + "/resources/common/Login.jsp");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
