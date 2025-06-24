72b709b4f362bdecccaf3b04238c6c14c793be46
f-niemiec <f.niemiec@studenti.unisa.it>
2025-06-24 22:47:21 +0200

    Finished ProductDS(?)

--- web-app/src/main/java/model/ProductDAO.java
+++ web-app/src/main/java/model/ProductDAO.java
@@ -5,9 +5,9 @@ import java.util.Collection;
 public interface ProductDAO {
 	boolean doSave(ProductBean product);
 	ProductBean doRetrieveByKey(int id);
-	Collection<ProductBean> doRetrieveAll();
-	boolean doDeleteByID(int id);
-	boolean doUpdatePrice(int id, int newPrice);
+	Collection<ProductBean> doRetrieveAll(String order);
+	boolean doDelete(int id);
+	boolean doUpdatePrice(int id, double newPrice);
 	boolean doUpdateTrend(int id);
 	boolean doUpdateQuantity(int id, int update);
 	boolean doUpdateNew(int id);
--- web-app/src/main/java/model/ProductDS.java
+++ web-app/src/main/java/model/ProductDS.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Collection;
+import java.util.LinkedList;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -61,8 +63,110 @@ public class ProductDS implements ProductDAO{
 		return bean;
 	}
 	
+	public Collection<ProductBean> doRetrieveAll(String order){
+		String query;
+		query = "SELECT * FROM " + TABLE_NAME;
+		Collection<ProductBean> products = new LinkedList<ProductBean>();
+		if(order!=null && !order.equals("")) {
+			query += "ORDER BY " + order;
+		}
+		try(Connection connection = ds.getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			ResultSet rs = preparedStatement.executeQuery();{
+				while(rs.next()) {
+					ProductBean bean = new ProductBean();
+					bean.setName(rs.getString("Nome"));
+					bean.setPrice(rs.getDouble("Prezzo"));
+					bean.setQuantity(rs.getInt("Quantita"));
+					bean.setType(rs.getString("Tipo"));
+					bean.setSize(rs.getString("Taglia"));
+					bean.setRecent(rs.getBoolean("Nuovo"));
+					bean.setTrend(rs.getBoolean("Tendenza"));
+					bean.setDescription(rs.getString("Descrizione"));
+					products.add(bean);
+				}
+			}
+			} catch (SQLException s) {
+			System.out.println("Si è verificato il seguente errore: " + s.getMessage());
+		}
+		return products;
+	}
 	
+	public boolean doDelete(int id) {
+		String query;
+		query = "DELETE FROM " + TABLE_NAME + " WHERE ID = ?";
+		int result = 0;
+		try(Connection connection = ds.getConnection()){
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setInt(1, id);
+			result = preparedStatement.executeUpdate();
+			} catch (SQLException s) {
+			System.out.println("Si è verificato il seguente errore: " + s.getMessage());
+		}
+		if(result != 0)
+			return true;
+		else 
+			return false;
+	}
 	
+	public boolean doUpdatePrice(int id, double newPrice) {
+	    String query = "UPDATE " + TABLE_NAME + " SET Prezzo = ? WHERE ID = ?";
+	    int result = 0;
+	    try (Connection connection = ds.getConnection()) {
+	        PreparedStatement preparedStatement = connection.prepareStatement(query);
+	        preparedStatement.setDouble(1, newPrice);
+	        preparedStatement.setInt(2, id);
+	        result = preparedStatement.executeUpdate();
+	    } catch (SQLException s) {
+	        System.out.println("Si è verificato il seguente errore nell'aggiornamento del prezzo: " + s.getMessage());
+	    }
+	    
+	    return result > 0;
+	}
+	
+	public boolean doUpdateQuantity(int id, int update) {
+	    String query = "UPDATE " + TABLE_NAME + " SET Quantita = Quantita + ? WHERE ID = ?";
+	    int result = 0;
+	    try (Connection connection = ds.getConnection()) {
+	        PreparedStatement preparedStatement = connection.prepareStatement(query);
+	        preparedStatement.setInt(1, update);
+	        preparedStatement.setInt(2, id);
+	        result = preparedStatement.executeUpdate();
+	    } catch (SQLException s) {
+	        System.out.println("Errore durante l'aggiornamento della quantità: " + s.getMessage());
+	    }
+
+	    return result > 0;
+	}
+	
+	public boolean doUpdateTrend(int id) {
+	    String query = "UPDATE " + TABLE_NAME + " SET Tendenza = NOT Tendenza WHERE ID = ?";
+	    int result = 0;
+	    try (Connection connection = ds.getConnection()) {
+	        PreparedStatement preparedStatement = connection.prepareStatement(query);
+	        preparedStatement.setInt(1, id);
+	        result = preparedStatement.executeUpdate();
+	    } catch (SQLException s) {
+	        System.out.println("Errore durante l'aggiornamento della tendenza: " + s.getMessage());
+	    }
+
+	    return result > 0;
+	}
+	
+	public boolean doUpdateNew(int id) {
+	    String query = "UPDATE " + TABLE_NAME + " SET Nuovo = NOT Nuovo WHERE ID = ?";
+	    int result = 0;
+	    try (Connection connection = ds.getConnection()) {
+	        PreparedStatement preparedStatement = connection.prepareStatement(query);
+	        preparedStatement.setInt(1, id);
+	        result = preparedStatement.executeUpdate();
+	    } catch (SQLException s) {
+	        System.out.println("Errore durante l'aggiornamento della tendenza: " + s.getMessage());
+	    }
+
+	    return result > 0;
+	}
+
 	
 	
 	private static DataSource ds;
