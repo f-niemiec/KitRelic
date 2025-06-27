d3406d113cb817e4848b10b7aab29c505186672e
f-niemiec <f.niemiec@studenti.unisa.it>
2025-06-27 22:50:12 +0200

    Added AddressBean

---
+++ web-app/src/main/java/model/AddressBean.java
@@ -0,0 +1,81 @@
+package model;
+
+import java.io.Serializable;
+
+public class AddressBean implements Serializable{
+	public AddressBean() {}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public TipoIndirizzo getAddress() {
+		return address;
+	}
+	
+	public void setAddress(TipoIndirizzo address) {
+		this.address = address;
+	}
+	
+	public boolean isActive() {
+		return active;
+	}
+	
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	public String getStreet() {
+		return street;
+	}
+	
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	
+	public String getProvince() {
+		return province;
+	}
+	
+	public void setProvince(String province) {
+		this.province = province;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+		
+	public int getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private TipoIndirizzo address;
+	private boolean active;
+	private String city;
+	private String street;
+	private String province;
+	private String country;
+	private int userId;
+}
---
+++ web-app/src/main/java/model/TipoIndirizzo.java
@@ -0,0 +1,6 @@
+package model;
+
+public enum TipoIndirizzo {
+	Billing,
+	Shipping,
+}
